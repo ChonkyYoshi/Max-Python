cba2c84125eeb289015b87211263ec853c82c9b2
Enzo Agosta <agosta.enzowork@gmail.com>
2023-07-01 11:50:18 +0200

    Added Doc2PDF Functions Added AcceptRevisions Functions Updated name and config.ini UPdated layout

--- Functions.py
+++ Functions.py
@@ -1,11 +1,13 @@
-def Upsave(FullPath):
+def split(FullPath):
 	from regex import match, findall
 
 	PathOnly = ''
 	for find in findall(r'[^\/]+?\/',FullPath):
 		PathOnly += find
 	FileOnly = match(r'(?r)[^\/]+',FullPath).group()
+	return(FullPath, PathOnly, FileOnly)
 
+def Upsave(FullPath):
 	match FullPath[-3:]:
 		case 'doc':
 			FullPath = Doc2Docx(FullPath, PathOnly, FileOnly)
@@ -13,35 +15,58 @@ def Upsave(FullPath):
 			FullPath = Ppt2Pptx(FullPath, PathOnly, FileOnly)
 		case 'xls':
 			FullPath = Xls2Xlsx(FullPath, PathOnly, FileOnly)
-	return(FullPath, PathOnly, FileOnly)
+	return(FullPath)
 
 def Doc2Docx(FullPath, PathOnly, FileOnly):
 	from win32com.client import DispatchEx
 
 	WordApp = DispatchEx('Word.Application')
 	Doc = WordApp.Documents.Open(FullPath)
-	Doc.SaveAs(PathOnly + FileOnly[:-4], FileFormat=12)
+	Doc.SaveAs(PathOnly + 'No_TC_' + FileOnly[:-4], FileFormat=12)
 	WordApp.Quit()
-	FullPath = PathOnly + FileOnly[:-3] + 'docx'
+	FullPath = PathOnly + 'No_TC_' + FileOnly[:-3] + 'docx'
 	return(FullPath)
+
+def Doc2PDF(FullPath, PathOnly, FileOnly, AccTC, RejTC, DelCom, Overwrite):
+	from win32com.client import DispatchEx
+
+	WordApp = DispatchEx('Word.Application')
+	Doc =  WordApp.Documents.Open(FullPath)
+
+	if Doc.Revisions.Count > 0 and AccTC:
+			Doc.AcceptAllRevisions()
+	elif Doc.Revisions.Count > 0 and RejTC:
+		Doc.RejectAllRevisions()
+	if Doc.Comments.Count > 0 and DelCom:
+			Doc.DeleteAllComments()
+	if Overwrite:
+		Doc.Save()
+	match FileOnly[-1]:
+		case 'x':
+			Doc.SaveAs(PathOnly + FileOnly[:-5], FileFormat=17)
+		case 'm':
+			Doc.SaveAs(PathOnly + FileOnly[:-5], FileFormat=17)
+		case 'c':
+			Doc.SaveAs(PathOnly + FileOnly[:-4], FileFormat=17)
+	WordApp.Quit()
 
 def Xls2Xlsx(FullPath, PathOnly, FileOnly):
 	from win32com.client import DispatchEx
 
 	XlApp = DispatchEx('Excel.Application')
 	Xl = XlApp.Workbooks.Open(FullPath)
-	Xl.SaveAs(PathOnly + FileOnly[:-4], FileFormat=51)
+	Xl.SaveAs(PathOnly + 'No_TC_' + FileOnly[:-4], FileFormat=51)
 	XlApp.Quit()
-	return(PathOnly + FileOnly[:-3] + 'xlsx')
+	return(PathOnly + 'No_TC_' + FileOnly[:-3] + 'xlsx')
 
 def Ppt2Pptx(FullPath, PathOnly, FileOnly):
 	from win32com.client import DispatchEx
 
 	PptApp = DispatchEx('PowerPoint.Application')
 	Ppt = PptApp.Presentations.Open(FullPath,0,0,0)
-	Ppt.SaveAs(PathOnly + FileOnly[:-4], FileFormat=24)
+	Ppt.SaveAs(PathOnly + 'No_TC_' + FileOnly[:-4], FileFormat=24)
 	PptApp.Quit()
-	return(PathOnly + FileOnly[:-3]+ 'pptx')
+	return(PathOnly + 'No_TC_' + FileOnly[:-3]+ 'pptx')
 
 def ExtractImages(FullPath, PathOnly, FileOnly):
 	import zipfile as zip
@@ -90,13 +115,13 @@ def FillCS(Tempdir, PathOnly, FileOnly):
 				Table.cell(2,0).merge(Table.cell(2,1))
 				Table.cell(3,0).text = 'Source'
 				Table.cell(3,1).text = 'Target'
-Table.cell(1,0).paragraphs[0].add_run().add_picture(PicRoot.replace('\\', '/') + '/' + pic, width=(CS.sections[0].page_width - (CS.sections[0].right_margin + CS.sections[0].left_margin)))
-				if FileOnly.endswith('pptx') or FileOnly.endswith('story'):
-					Locations = LocateImage(Tempdir, pic)
-					for location in Locations:
-						Table.cell(2,0).add_paragraph(location, style='List Bullet')
-				CS.add_section()
-				CS.add_section()
+				Table.cell(1,0).paragraphs[0].add_run().add_picture(PicRoot.replace('\\', '/') + '/' + pic, width=(CS.sections[0].page_width - (CS.sections[0].right_margin + CS.sections[0].left_margin)))
+			if FileOnly.endswith('pptx') or FileOnly.endswith('story'):
+				Locations = LocateImage(Tempdir, pic)
+				for location in Locations:
+					Table.cell(2,0).add_paragraph(location, style='List Bullet')
+					CS.add_section()
+					CS.add_section()
 	CS.save(PathOnly + 'Contact Sheets/CS_' + FileOnly + '.docx')
 	rmtree(Tempdir)
 
@@ -111,7 +136,7 @@ def LocateImage(TempDir, ImageName):
 				relstr = str(open(Relroot + '\\' + rel).read())
 				if search(ImageName[:-4], relstr):
 					Locations.append(rel[:5] + ' ' + rel[5:rel.find('.')])
-return(Locations)
+	return(Locations)
 
 def BilTables(FullPath, PathOnly, FileOnly, BasPath):
 	from win32com.client import DispatchEx
@@ -130,4 +155,28 @@ def BilText(FullPath, BasPath):
 	Doc = WordApp.Documents.Open(FullPath)
 	Doc.VBProject.VBComponents.Import(BasPath)
 	Doc.Application.Run('Bil_Text')
+	WordApp.Quit()
+
+def AcceptRevisions(FullPath, PathOnly, FileOnly, AccTC, RejTC, DelCom, Overwrite):
+	from win32com.client import DispatchEx
+
+	WordApp = DispatchEx('Word.Application')
+	Doc =  WordApp.Documents.Open(FullPath)
+
+	if Doc.Revisions.Count > 0 and AccTC:
+			Doc.AcceptAllRevisions()
+	elif Doc.Revisions.Count > 0 and RejTC:
+		Doc.RejectAllRevisions()
+	if Doc.Comments.Count > 0 and DelCom:
+			Doc.DeleteAllComments()
+	if Overwrite:
+		Doc.Save()
+	else:
+		match FileOnly[-1]:
+			case 'x':
+				Doc.SaveAs(PathOnly + 'NoRev_' + FileOnly[:-5], FileFormat=12)
+			case 'm':
+				Doc.SaveAs(PathOnly + 'NoRev_' + FileOnly[:-5], FileFormat=12)
+			case 'c':
+				Doc.SaveAs(PathOnly + 'NoRev_' + FileOnly[:-4], FileFormat=0)
 	WordApp.Quit()
--- Main.py
+++ Main.py
@@ -4,67 +4,94 @@ from configparser import ConfigParser
 from os.path import isfile, abspath
 from os import remove
 
+#Get description from config
 config = ConfigParser()
 config.read('config.ini')
 
 #Functions started when clicking on "Run"
 def Contact_Sheet(PathInput):
-	FullPath, PathOnly, FileOnly = fn.Upsave(PathInput)
+	FullPath, PathOnly, FileOnly = fn.split(PathInput)
+	MainWindow['---PBarFile---'].update(FileOnly)
+	MainWindow['---PBarFileStep---'].update('Upsaving to Office 2007 format')
+	MainWindow['---PBar---'].update((index+1/5)/len(PathList)*100)
+	FullPath = fn.Upsave(FullPath)
 	MainWindow['---PBarFile---'].update(FileOnly)
 	MainWindow['---PBarFileStep---'].update('Extracting Images')
-	MainWindow['---PBar---'].update((index+1/4)/len(PathList)*100)
+	MainWindow['---PBar---'].update((index+2/5)/len(PathList)*100)
 	MainWindow.refresh()
 	fn.ExtractImages(FullPath, PathOnly, FileOnly)
 	MainWindow['---PBarFileStep---'].update('cleaning up jpeg and jpg')
-	MainWindow['---PBar---'].update((index+2/4)/len(PathList)*100)
+	MainWindow['---PBar---'].update((index+3/5)/len(PathList)*100)
 	MainWindow.refresh()
 	fn.CleanTempDir(PathOnly + 'Temp')
 	MainWindow['---PBarFileStep---'].update('Filling in Contact Sheet')
-	MainWindow['---PBar---'].update((index+3/4)/len(PathList)*100)
+	MainWindow['---PBar---'].update((index+4/5)/len(PathList)*100)
 	MainWindow.refresh()
 	fn.FillCS(PathOnly + 'Temp', PathOnly, FileOnly)
 
 def Bilingual(PathInput):
-	FullPath, PathOnly, FileOnly = fn.Upsave(PathInput)
+	FullPath, PathOnly, FileOnly = fn.split(PathInput)
+	MainWindow['---PBarFile---'].update(FileOnly)
+	MainWindow['---PBarFileStep---'].update('Upsaving to Office 2007 format')
+	MainWindow['---PBar---'].update((index+1/5)/len(PathList)*100)
+	FullPath = fn.Upsave(FullPath)
 	MainWindow['---PBarFile---'].update(FileOnly)
 	MainWindow['---PBarFileStep---'].update('Processing tables')
-	MainWindow['---PBar---'].update((index + 1/4)/len(PathList)*100)
+	MainWindow['---PBar---'].update((index + 2/5)/len(PathList)*100)
 	FullPath = fn.BilTables(FullPath.replace('/','\\\\'), PathOnly, FileOnly, abspath('Bas Files\Bil.bas').replace('\\', '\\\\'))
 	MainWindow.refresh()
 	MainWindow['---PBarFile---'].update(FileOnly)
 	MainWindow['---PBarFileStep---'].update('Processing regular text')
-	MainWindow['---PBar---'].update((index + 2/4)/len(PathList)*100)
+	MainWindow['---PBar---'].update((index + 3/5)/len(PathList)*100)
 	fn.BilText(FullPath.replace('/','\\\\'), abspath('Bas Files\Bil.bas').replace('\\', '\\\\'))
 	MainWindow['---PBarFile---'].update(FileOnly)
 	MainWindow['---PBarFileStep---'].update('Removing Temp file')
-	MainWindow['---PBar---'].update((index + 3/4)/len(PathList)*100)
+	MainWindow['---PBar---'].update((index + 4/5)/len(PathList)*100)
 	MainWindow.refresh()
 	remove(FullPath)
+
+def Doc2PDF(PathInput, AccTC, RejTC, DelCom, Overwrite):
+	FullPath, PathOnly, FileOnly = fn.split(PathInput)
+	MainWindow['---PBarFile---'].update(FileOnly)
+	MainWindow['---PBarFileStep---'].update('Saving as PDF')
+	MainWindow['---PBar---'].update(index/len(PathList)*100)
+	FullPath = fn.Doc2PDF(FullPath, PathOnly, FileOnly, AccTC, RejTC, DelCom, Overwrite)
+
+def AcceptRevisions(PathInput, AccTC, RejTC, DelCom, Overwrite):
+	FullPath, PathOnly, FileOnly = fn.split(PathInput)
+	MainWindow['---PBarFile---'].update(FileOnly)
+	MainWindow['---PBarFileStep---'].update('Accepting Revisions')
+	MainWindow['---PBar---'].update(index/len(PathList)*100)
+	FullPath = fn.AcceptRevisions(FullPath, PathOnly, FileOnly, AccTC, RejTC, DelCom, Overwrite)
+
 #GUI layout and creation
 TopText = [
-	[gui.Text(text='Welcome to Max GUI!\nThis utility lets you run the different functions of Max (also knows as the Watched Folders) anywhere you want.\nSimply choose a function from the sidebar on the left, click on Browse, select all the files you want to process and click on run.',justification='center')],
+	[gui.Text(text=str(config['Descriptions']['TopText']),justification='center')],
 	[gui.HorizontalSeparator()]
 ]
 
 Sidebar = [
 	[gui.Button(button_text='Contact Sheet',size=15)],
 	[gui.Button(button_text='Bilingual Table',size=15)],
-	[gui.Button(button_text='ChExcel',size=15)],
-	[gui.Button(button_text='Other',size=15)]
+	[gui.Button(button_text='Word to PDF',size=15)],
+	[gui.Button(button_text='Accept Revisions',size=15)]
 ]
 
-MainCanvas = [
-	[gui.InputText(default_text='',size=75,key='---PathInput---'),gui.FilesBrowse(target='---PathInput---')],
-	[gui.Text(text='',key='---Description---',size=(50,10))],
+Rightlayout = [
+	[gui.InputText(default_text='',key='---PathInput---',),gui.FilesBrowse(target='---PathInput---')],
+	[gui.Text(text='',key='---Description---')],
 	[gui.Submit(button_text='Run',size=15,visible=False,key='---Run---')],
-	[gui.ProgressBar(max_value=100,orientation='horizontal',size=(48,20),bar_color=('green','white'),key='---PBar---',visible=False)],
+	[gui.Checkbox(text='Accept Revisions',auto_size_text=True,key='---AcceptTC---',visible=False), gui.Checkbox(text='Reject Revisions',auto_size_text=True,key='---RejectTC---',visible=False)],
+	[gui.Checkbox(text='DeleteComments',auto_size_text=True,key='---DeleteCom---',visible=False)],
+	[gui.Checkbox(text='Overwrite',auto_size_text=True,key='---Overwrite---',visible=False)],
+	[gui.ProgressBar(max_value=100,orientation='horizontal',size=(50,20),bar_color=('green','white'),key='---PBar---',visible=False)],
 	[gui.Text(text='',key='---PBarFile---')],
 	[gui.Text(text='',key='---PBarFileStep---')]
 ]
 
-layout = [[TopText, gui.Column(Sidebar, vertical_scroll_only=True, scrollable=True,expand_y=True), gui.VSeparator(), gui.Column(MainCanvas)]]
+layout = [[TopText, gui.Column(Sidebar, vertical_scroll_only=True, scrollable=True,expand_y=True), gui.VSeparator(), gui.Column(Rightlayout)]]
 
-MainWindow = gui.Window('Max GUI', layout)
+MainWindow = gui.Window('Prep ToolKit', layout,size=(780,400))
 
 #read events from GUI and start main loop
 while True:
@@ -74,17 +101,13 @@ while True:
 			break
 		case gui.WIN_CLOSED:
 			break
-		case 'Contact Sheet':
-			MainWindow['---Description---'].update(config['Descriptions']['Contact_Sheet'])
-			MainWindow['---Run---'].update(visible=True)
-			FunctionName = 'Contact_Sheet'
-		case 'Bilingual Table':
-			MainWindow['---Description---'].update(config['Descriptions']['Bilingual_Table'])
-			MainWindow['---Run---'].update(visible=True)
-			FunctionName = 'Bilingual_Table'
 		case '---Run---':
+			if MainWindow['---AcceptTC---'].get() == True and MainWindow['---RejectTC---'].get() == True:
+				gui.popup_error('Both Accept and Reject TC are selected!\nPlease check only of the options and try again.',title='Both accept and reject selected')
+				break
 			PathList = values['---PathInput---'].split(';')
 			MainWindow['---PBar---'].update(visible=True)
+			
 			for index, PathInput in enumerate(PathList):
 				if not isfile(PathInput):
 					gui.popup_error(f'the following file:\n{PathInput}\n is not a valid file!\n Skipping this file', title='Invalid file error', auto_close=True, auto_close_duration=5, keep_on_top=True, modal=True)
@@ -94,8 +117,44 @@ while True:
 						Contact_Sheet(PathInput)
 					case 'Bilingual_Table':
 						Bilingual(PathInput)
+					case 'Doc2PDF':
+						Doc2PDF(PathInput, MainWindow['---AcceptTC---'].get(), MainWindow['---RejectTC---'].get(), MainWindow['---DeleteCom---'].get(), MainWindow['---Overwrite---'].get())
+					case 'Accept Revisions':
+						AcceptRevisions(PathInput, MainWindow['---AcceptTC---'].get(), MainWindow['---RejectTC---'].get(), MainWindow['---DeleteCom---'].get(), MainWindow['---Overwrite---'].get())
 			MainWindow['---PBar---'].update(100)
 			MainWindow['---PBarFile---'].update('')
 			MainWindow['---PBarFileStep---'].update('Done!')
+		case 'Contact Sheet':
+			MainWindow['---Description---'].update(config['Descriptions']['Contact_Sheet'])
+			MainWindow['---Run---'].update(visible=True)
+			MainWindow['---AcceptTC---'].update(visible=False)
+			MainWindow['---RejectTC---'].update(visible=False)
+			MainWindow['---DeleteCom---'].update(visible=False)
+			MainWindow['---Overwrite---'].update(visible=False)
+			FunctionName = 'Contact_Sheet'
+		case 'Bilingual Table':
+			MainWindow['---Description---'].update(config['Descriptions']['Bilingual_Table'])
+			MainWindow['---Run---'].update(visible=True)
+			MainWindow['---AcceptTC---'].update(visible=False)
+			MainWindow['---RejectTC---'].update(visible=False)
+			MainWindow['---DeleteCom---'].update(visible=False)
+			MainWindow['---Overwrite---'].update(visible=False)
+			FunctionName = 'Bilingual_Table'
+		case 'Word to PDF':
+			MainWindow['---Description---'].update(config['Descriptions']['Doc2PDF'])
+			MainWindow['---Run---'].update(visible=True)
+			MainWindow['---AcceptTC---'].update(visible=True)
+			MainWindow['---RejectTC---'].update(visible=True)
+			MainWindow['---DeleteCom---'].update(visible=True)
+			MainWindow['---Overwrite---'].update(visible=True)
+			FunctionName = 'Doc2PDF'
+		case 'Accept Revisions':
+			MainWindow['---Description---'].update(config['Descriptions']['Accept_Revisions'])
+			MainWindow['---Run---'].update(visible=True)
+			MainWindow['---AcceptTC---'].update(visible=True)
+			MainWindow['---RejectTC---'].update(visible=True)
+			MainWindow['---DeleteCom---'].update(visible=True)
+			MainWindow['---Overwrite---'].update(visible=True)
+			FunctionName = 'Accept Revisions'
 
 MainWindow.close()
